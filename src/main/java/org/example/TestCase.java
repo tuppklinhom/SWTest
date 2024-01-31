a5a34c016e5012e271780759a61f8c49f9effa79
thanapornarun <thanaporn.sael@ku.th>
2024-01-31 18:28:26 +0700

    update

--- src/main/java/org/example/TestCase.java
+++ src/main/java/org/example/TestCase.java
@@ -6,7 +6,9 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class TestCase {
-    private String filename;
+    private String filename, testerName;
+    private int passCount = 0;
+    private int notPassCount = 0;
     private int count = 0;
 
     public TestCase(){
@@ -15,27 +17,62 @@ public class TestCase {
 
         System.out.println("Enter Filename: ");
         filename = userInput.nextLine();
+        System.out.println("Enter Tester Name: ");
+        testerName = userInput.nextLine();
 
         /** Test part **/
         TestCal(4 , 8 ,32);
         TestCal(7, 10 ,70);
+        TestCal(6, 8, 48);
+        TestCal(9, 12, 108);
+        TestCal(10, 10, 100);
         TestCal(10, 12, 120);
+        TestCal(11, 10, -1);
+        TestCal(8, 13, -1);
+        TestCal(3, 10, -1);
+        TestCal(5, 7, -1);
+
+        try {
+            FileWriter fw = new FileWriter(filename + ".txt", true);
+            String testCount =
+                    "\nTotal Pass: " + String.valueOf(passCount) +
+                            "\nTotal Not Pass: " + String.valueOf(notPassCount) +
+                            "\n===============================";
+
+            fw.write(testCount);
+            fw.close();
+        }catch(Exception e) {
+            e.getStackTrace();
+            }
 
         /***************/
     }
 
     private void TestCal(int x, int y, int expectedResult){
+        String passStatus = "";
+
         long start = System.currentTimeMillis();
         count++;
         SquareCalculator squareCalculator = new SquareCalculator();
         int ans = squareCalculator.cal(x, y);
 
+        if (ans == expectedResult) {
+            passStatus = "Pass";
+            passCount++;
+        }
+        else {
+            passStatus = "Not Pass";
+            notPassCount++;
+        }
+
         String testCase;
 
         testCase ="[Test case " + count + " ]" +
-                 "\nInput: x=" + String.valueOf(squareCalculator.getX()) + ", y=" + String.valueOf(squareCalculator.getY()) +
+                "\nTest By: " + testerName +
+                "\nInput: x=" + String.valueOf(squareCalculator.getX()) + ", y=" + String.valueOf(squareCalculator.getY()) +
                 "\nExpected result: " + expectedResult +
-                "\nActual Result: " + ans;
+                "\nActual Result: " + ans +
+                "\nStatus: " + passStatus;
         long finished = System.currentTimeMillis();
         testCase = testCase + "\nExecution Time: " + (finished - start) + " ms\n";
         try {
@@ -50,7 +87,6 @@ public class TestCase {
             fw.write(testFinish);
             fw.write("===============================\n");
             System.out.println("Successfully written");
-
                 // close the file
             fw.close();
         } catch (Exception e) {
